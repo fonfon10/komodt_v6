fa01b460a7ca7f9d7a8d3ba725088946f244ae2e
Serge Lafontaine <slafontaine10@gmail.com>
2020-05-16 16:06:11 -0400

    remove confirmable and started seed file

--- db/schema.rb
+++ db/schema.rb
@@ -41,11 +41,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_185950) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index "\"confirmation_token\"", name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+
+
 Role.delete_all
 #Roles
 role_types = ['Admin', 'Operator', 'User']
@@ -14,5 +16,25 @@ role_types = ['Admin', 'Operator', 'User']
 role_types.each do |r|
 	Role.create!(name: r)
 end
+
+role_type_Admin = Role.first
+
+
+User.delete_all
+user_Serge = User.create!(firstname:'Serge', lastname:'Lafontaine', email: 'slafontaine10@gmail.com', password: 'numb10', password_confirmation: 'numb10', role: role_type_Admin)
+
+
+
+
+
+Supplier.delete_all
+#Suppliers
+supplier_types = ['Metro', 'Loblaws', "Costco"]
+
+supplier_types.each do |s|
+	Supplier.create!(name: s, user:user_Serge)
+end
+
+
 
 
